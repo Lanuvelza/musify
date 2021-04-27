3cc9ed1543f11613b5768371651cddcb468db6de
Ian <ianlam1221@hotmail.com>
2021-04-26 22:08:28 -0700

    added login authentication

--- spotify-clone/src/components/Login.js
+++ spotify-clone/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./styles/Login.css"
+import "./styles/Login.css";
+import {loginUrl} from "./spotify/spotify";
 
 function Login() {
   return (
@@ -9,7 +10,7 @@ function Login() {
         src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
         alt="Spotify logo"
       />
-      <a href="#">LOGIN WITH SPOTIFY</a>
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   </div>
   );
