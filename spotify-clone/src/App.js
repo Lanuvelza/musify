b60615715229204e6812c2533c31dad39be84d57
Ian <ianlam1221@hotmail.com>
2021-04-27 00:23:28 -0700

    added reducer and datalayer context

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Login from "./components/Login"
 import { getTokenFromUrl } from "./components/spotify/spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./contexts/DataLayer";
+import Player from "./components/Player";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(); 
+  const [{ user, token }, dispatch] = useDataLayerValue(); 
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -15,16 +17,38 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      console.log("[token]", token); 
       spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+      spotify.getUserPlaylists()
+      .then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists
+        });
+      });
+      spotify.getPlaylist("37i9dQZF1E34Ucml4HHx1w")
+      .then((playlist) => {
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY", 
+          discover_weekly: playlist, 
+        });
+      });
     }
-
-    console.log("token", token);
   }, []);
 
   return (
   <div className="app">
-    <Login />
+    {token ? <Player spotify={spotify} /> : <Login />}
   </div>
   );
 }
