60209abd5a365c1c93f78e4278422982fd9b2189
Ian <ianlam1221@hotmail.com>
2021-04-27 16:20:19 -0700

    added footer component

--- spotify-clone/src/components/player/Player.js
+++ spotify-clone/src/components/player/Player.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/Player.css";
 import Sidebar from "./Sidebar";
 import Body from "./Body";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
   return (
@@ -10,8 +11,9 @@ function Player({ spotify }) {
       <Sidebar />
       <Body spotify={spotify} />
     </div>
+    <Footer />
   </div>
-  )
+  );
 }
 
 export default Player;
