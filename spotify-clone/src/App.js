2ba0e1edf9aac9dbc09d021e5396c77d408ade15
Ian <ianlam1221@hotmail.com>
2021-04-26 22:26:57 -0700

    stored the access token in state

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./components/Login"
+import { getTokenFromUrl } from "./components/spotify/spotify";
 
 function App() {
+  const [token, setToken] = useState(); 
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ""; 
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
   return (
   <div className="app">
     <Login />
