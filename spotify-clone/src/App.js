5ed439e6b0bd38eb818b2638b38117b1e5c2d557
Ian <ianlam1221@hotmail.com>
2021-04-26 18:09:47 -0700

    added login component

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import './App.css';
+import Login from "./components/Login"
 
 function App() {
-  return <div className="app">React App</div>;
+  return (
+  <div className="app">
+    <Login />
+  </div>
+  );
 }
 
 export default App;
---
+++ spotify-clone/src/components/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/Login.css"
+
+function Login() {
+  return (
+  <div className="login">
+    <div className="loginCard">
+      <img 
+        src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+        alt="Spotify logo"
+      />
+      <a href="#">LOGIN WITH SPOTIFY</a>
+    </div>
+  </div>
+  );
+}
+
+export default Login;
