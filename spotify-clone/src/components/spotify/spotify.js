c55a69342665553ea9194019018b695305ddc5c7
Ian <ianlam1221@hotmail.com>
2021-04-26 22:10:25 -0700

    added spotify configuration

---
+++ spotify-clone/src/components/spotify/spotify.js
@@ -0,0 +1,14 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+
