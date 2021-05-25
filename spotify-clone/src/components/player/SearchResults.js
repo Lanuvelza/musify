c38f5eb55e32a4d1c1669fad9440448e2676d7a1
Ian <ianlam1221@hotmail.com>
2021-05-24 21:47:19 -0700

    updated stylings

--- spotify-clone/src/components/player/SearchResults.js
+++ spotify-clone/src/components/player/SearchResults.js
@@ -1,5 +1,4 @@
 import React from 'react'; 
-import { useDataLayerValue } from '../../contexts/DataLayer';
 import { Avatar, makeStyles } from "@material-ui/core";
 import "./styles/SearchResults.css";
 
@@ -7,31 +6,25 @@ const useStyles = makeStyles({
   avatar: {
     height: '200px',
     width: '200px',
-    borderStyle: 'solid',
-    borderColor: 'white', 
-    borderWidth: '3px',
     boxShadow: '5px 8px 15px black',
-    margin: '15px',
-    justifyContent: 'center',
   }
 })
 
 function SearchResults({artists}) {
-  // const [{artists}, dispatch] = useDataLayerValue();
-  console.log(artists);
-
   const classes = useStyles();
 
   const searchedArtists = artists.items.map(artist => {
     return (
       <div key={artist.id} className="results__itemBody">
-        <Avatar 
-          src={artist.images.length ? artist.images[0].url : null} 
-          alt={artist.name} 
-          className={classes.avatar}
-        />
+        <div className="results__itemAvatar">
+          <Avatar 
+            src={artist.images.length ? artist.images[0].url : null} 
+            alt={artist.name} 
+            className={classes.avatar}
+          />
+        </div>
         <div className="results__itemInfo">
-          {artist.name}
+          <p>{artist.name}</p>
         </div>
       </div>
     )
@@ -41,9 +34,6 @@ function SearchResults({artists}) {
 
   return (
     <div>
-      {/* <div className="results__header">
-        <h1>Search Results</h1>
-      </div> */}
       <div className="results__body">
         {searchedArtists}
       </div>
