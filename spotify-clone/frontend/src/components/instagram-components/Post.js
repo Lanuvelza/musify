14d6fec45bffa64799db2bad7b422dd845ed44fb
Ian <ianlam1221@hotmail.com>
2021-06-24 14:15:01 -0700

    adjusted stylings for the displayed post

--- spotify-clone/frontend/src/components/instagram-components/Post.js
+++ spotify-clone/frontend/src/components/instagram-components/Post.js
@@ -11,16 +11,23 @@ function Post() {
   const timestamp = date.toISOString();
 
   return (
-    <div className="post__body">
-      <img 
-        src={`https://workers.iantyylam.workers.dev/${latest__post?.node?.display_url}`}
-        alt={latest__post?.node?.shortcode}
-        className={"post__picture"}
-      />
-      <div className="post__caption">
-        {latest__post?.node?.edge_media_to_caption?.edges?.[0]?.node?.text}
-        <hr />
-        <TimeAgo date={timestamp} />
+    <div className="post">
+      <div className="post__header">
+        <h2>Latest Post</h2>
+      </div>
+      <div className="post__body">
+        <img 
+          src={`https://workers.iantyylam.workers.dev/${latest__post?.node?.display_url}`}
+          alt={latest__post?.node?.shortcode}
+          className={"post__picture"}
+        />
+        <div className="post__caption">
+          <p>{latest__post?.node?.edge_media_to_caption?.edges?.[0]?.node?.text}</p>
+          <hr />
+          <div className="post__timestamp">
+            <TimeAgo date={timestamp} />
+          </div>
+        </div>
       </div>
     </div>
   )
